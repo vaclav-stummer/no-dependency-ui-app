cefef0c986173c56160b625e806efba56bd54b13
Vaclav Stummer <vaclav@oakslab.com>
2021-08-24 21:42:10 +0200

    scss enhancement

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,3 @@
-import { formData } from './forms'
-
 import './style.scss'
 
 console.info('App started.')
-
-const form = document.querySelector('form') as HTMLFormElement
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault()
-
-  const data = formData(form)
-
-  console.log(data)
-})
