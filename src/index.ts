e82945990f428dcc0892b6f3038d9da5aa5c418e
Vaclav Stummer <vaclav@oakslab.com>
2021-08-26 23:36:23 +0200

    drag & drop prototype

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
+/* Libraries */
+import './libraries/dragAndDrop'
+
+/* Components */
 import PageLayout from './components/PageLayout'
 import PageHeader from './components/PageHeader'
 import PageContent from './components/PageContent'
 import LeftSideMenu from './components/LeftSideMenu'
-import ContentItem from './components/ContentItem'
 
 import './styles/style.scss'
 
@@ -13,4 +16,3 @@ PageLayout
 PageHeader
 PageContent
 LeftSideMenu
-ContentItem
