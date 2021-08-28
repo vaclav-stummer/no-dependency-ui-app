b376d2f2216036c2daed66924c14befdca6953d2
Vaclav Stummer <vaclav@oakslab.com>
2021-08-28 14:31:22 +0200

    content component enhancement

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import './libraries/dragAndDrop'
 
 /* Components */
 import PageLayout from './components/PageLayout'
-import PageContent from './components/PageContent'
 // TODO: Nice to have find out how to click trough to the exact .html file
 import Header from './components/Header/index.html'
 import LeftSideMenu from './components/LeftSideMenu/index.html'
@@ -18,11 +17,11 @@ window.onload = function () {
   const content = document?.querySelector('.content-section')
 
   root?.insertAdjacentHTML('afterbegin', Header)
+
   content?.insertAdjacentHTML('afterbegin', LeftSideMenu)
 }
 
 // TODO:Find out how to properly initialize components
 PageLayout
-PageContent
 
 console.info('App started.')
