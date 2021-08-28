7b4ef32e2d51cec8d1ab116d766d11cd120b619b
Vaclav Stummer <vaclav@oakslab.com>
2021-08-28 14:19:55 +0200

    left side menu component enhancement

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,10 @@ import './libraries/dragAndDrop'
 
 /* Components */
 import PageLayout from './components/PageLayout'
-import PageHeader from './components/PageHeader'
 import PageContent from './components/PageContent'
-import LeftSideMenu from './components/LeftSideMenu'
 // TODO: Nice to have find out how to click trough to the exact .html file
 import Header from './components/Header/index.html'
+import LeftSideMenu from './components/LeftSideMenu/index.html'
 
 import './styles/style.scss'
 
@@ -16,13 +15,14 @@ const root = document?.getElementById('root')
 window.onload = function () {
   if (!root) return
 
+  const content = document?.querySelector('.content-section')
+
   root?.insertAdjacentHTML('afterbegin', Header)
+  content?.insertAdjacentHTML('afterbegin', LeftSideMenu)
 }
 
 // TODO:Find out how to properly initialize components
 PageLayout
-PageHeader
 PageContent
-LeftSideMenu
 
 console.info('App started.')
